afdf3b7342e8d7256ff7ed43d392b36ba5785b52
Bilal Arshad <bilal.arshad@10pearls.com>
2024-12-27 12:04:59 +0500

    Added API testing folder only

--- test-constants/constants.js
+++
@@ -1,4 +0,0 @@
-export const URL = 'https://demoblaze.com/'
-export const USERNAME = 'bilalq3-10p2';
-export const PASSWORD = '12345';
-export const apiBaseUrl = 'https://restful-booker.herokuapp.com';
--- tests/playwright-api-testing/get-call.spec.js
+++ tests/playwright-api-testing/get-call.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('should get all the booking details', async({request})=> {
-    const response = await request.get("https://restful-booker.herokuapp.com/booking/3800");
+    const response = await request.get("https://restful-booker.herokuapp.com/booking");
     console.log(await response.json());
     expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(200);
